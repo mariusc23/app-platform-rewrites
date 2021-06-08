018769dc38edff8bcefe4df4d44a260a016c040b
Marius Craciunoiu <marius@uplift.ltd>
2021-06-08 13:31:20 -0600

    Send app name in response

--- app1/index.js
+++ app1/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(function (req, res) {
-  res.json(req.originalUrl);
+  res.send("app1: " + req.originalUrl);
 });
 
 app.listen(process.env.PORT || 8080);
